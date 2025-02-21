81a671bebf731b1b5f9ed32c285b50031ffe19f6
jseol0 <79302243+jseol0@users.noreply.github.com>
2025-02-21 11:01:34 +0900

    repo name change

--- LikeLion3/LikeLion3/Program.cs
+++ LikeLion3/LikeLion3/Program.cs
@@ -15,17 +15,31 @@ namespace LikeLion3
             //att : 56.7
             //캐릭터이름 : ??
             //등급 : s;
-            Console.WriteLine("status");
-
+            
             string name = "Alice";
             int hp = 100;
             double att = 56.7;
             char grade = 's';
 
+            // 1. 문자열 보간법
+            // 연산이 더 빠르고 효율적
+            Console.WriteLine("status");
+
             Console.WriteLine($"name : {name}");
             Console.WriteLine($"hp : {hp}");
             Console.WriteLine($"att : {att}");
             Console.WriteLine($"grade : {grade}");
+
+            Console.Write("\n\n\n");
+
+            // 2. + 연산자
+            // + 연산자는 문자열을 계속 새로운 문자열로 할당하기 때문에, 성능이 조금 떨어짐
+            Console.WriteLine("status");
+
+            Console.WriteLine("name : " + name);
+            Console.WriteLine("hp : " + hp);
+            Console.WriteLine("att : " + att);
+            Console.WriteLine("grade : " + grade);
         }
     }
 }
--- Likelion2/Likelion2/Program.cs
+++ Likelion2/Likelion2/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Likelion2
+namespace LikeLion2
 {
     internal class Program
     {
