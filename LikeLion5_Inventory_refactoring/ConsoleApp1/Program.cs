6ff9968d23c3953667e289bd00af3e8091181ac1
jseol0 <79302243+jseol0@users.noreply.github.com>
2025-02-27 17:50:21 +0900

    page43_함수구조체

---
+++ LikeLion5_Inventory_refactoring/ConsoleApp1/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LikeLion5_Inventory
+{
+
+    internal class Program
+    {
+        struct Inventory
+        {
+            public int MAX_ITEMS;
+            public string[] itemNames;
+            public int[] itemCounts;
+
+            public Inventory(int size)
+            {
+                MAX_ITEMS = size;
+                itemNames = new string[MAX_ITEMS];
+                itemCounts = new int[MAX_ITEMS];
+            }
+
+            //아이템 추가 함수
+            public void AddItem(string name, int count)
+            {
+                for (int i = 0; i < MAX_ITEMS; i++)
+                {
+                    if (itemNames[i] == name)  //이미 있는 아이템이면 개수 증가
+                    {
+                        itemCounts[i] += count;
+                        return;
+                    }
+                }
+
+                //빈 슬롯에 새로운 아이템 추가
+                for (int i = 0; i < MAX_ITEMS; i++)
+                {
+                    if (itemNames[i] == null)
+                    {
+                        itemNames[i] = name;
+                        itemCounts[i] = count;
+                        return;
+                    }
+                }
+                Console.WriteLine("인벤토리가 가득 찼습니다.");
+            }
+
+            //아이템 제거 함수
+            public void RemoveItem(string name, int count)
+            {
+                for (int i = 0; i < MAX_ITEMS; i++)
+                {
+                    if (itemNames[i] == name) //이름하고 같은지
+                    {
+                        if (itemCounts[i] >= count) //개수가 충분하면 차감
+                        {
+                            itemCounts[i] -= count;
+                            if (itemCounts[i] == 0) //개수가 0이면 삭제
+                            {
+                                itemNames[i] = null;
+                            }
+                            return;
+                        }
+                        else
+                        {
+                            Console.WriteLine("아이템 개수가 부족합니다!");
+                            return;
+                        }
+                    }
+                }
+
+                Console.WriteLine("아이템을 찾을 수 없습니다!");
+
+            }
+
+            //인벤토리 출력 함수
+            public void ShowInventory()
+            {
+                Console.WriteLine("현재 인벤토리 : ");
+                bool isEmpty = true;
+
+                for (int i = 0; i < MAX_ITEMS; i++)
+                {
+                    if (itemNames[i] != null)
+                    {
+                        Console.WriteLine($"{itemNames[i]} (x{itemCounts[i]})");
+                        isEmpty = false;
+                    }
+                }
+
+                if (isEmpty)
+                {
+                    Console.WriteLine("인벤토리가 비어 있습니다.");
+                }
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            Inventory inven = new Inventory(10);
+            //테스트 : 아이템 추가
+            inven.AddItem("포션", 5);
+            inven.AddItem("칼", 1);
+            inven.AddItem("포션", 3); //포션 개수 추가
+
+            inven.ShowInventory();
+
+            //아이템 사용
+            Console.WriteLine("포션 2개 사용");
+            inven.RemoveItem("포션", 2);
+            inven.ShowInventory();
+
+            //테스트 : 없는 아이템 제거
+            Console.WriteLine("방패 1개 제거 시도");
+            inven.RemoveItem("방패", 1);
+
+            inven.ShowInventory();
+
+            //테스트: 모든 포션 제거
+            Console.WriteLine("포션 6개 사용(초과 사용 테스트)");
+            inven.RemoveItem("포션", 6);
+            inven.ShowInventory();
+        }
+    }
+}
---
+++ LikeLion5_ShootingGame_refactoring/ConsoleApp2/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LikeLion5_ShootingGame1
+{
+    struct Point
+    {
+        public int X, Y;
+        public Point(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        //public int X { get { return x; } set { x = value; } }
+        //public int Y { get { return y; } set { y = value; } }
+    }
+
+    class Program
+    {
+        static ConsoleKeyInfo keyInfo;
+        static Point playerPos = new Point(0, 12);
+
+        static string[] playerString = new string[] {
+                "->",
+                ">>>",
+                "->"
+        };
+
+        static Stopwatch stopwatch = new Stopwatch();
+        static long prevSecond, currentSecond;
+
+        static void ReadInput(out bool isEscape)
+        {
+            isEscape = false;
+            if (Console.KeyAvailable)
+            {
+                keyInfo = Console.ReadKey(true);
+                switch (keyInfo.Key)
+                {
+                    case ConsoleKey.UpArrow: if (playerPos.Y > 0) playerPos.Y--; break;
+                    case ConsoleKey.DownArrow: if (playerPos.Y < Console.WindowHeight - 4) playerPos.Y++; break;
+                    case ConsoleKey.LeftArrow: if (playerPos.X > 0) playerPos.X--; break;
+                    case ConsoleKey.RightArrow: if (playerPos.X < Console.WindowWidth - 3) playerPos.X++; break;
+                    case ConsoleKey.Spacebar:
+                        Console.SetCursorPosition(playerPos.X + 2, playerPos.Y + 1);
+                        Console.Write("미사일키");
+                        break;
+                    case ConsoleKey.Escape: isEscape = true; break;// ESC 키로 종료
+                }
+            }
+        }
+
+        static void Render()
+        {
+            if (currentSecond - prevSecond >= 100)
+            {
+                Console.Clear();
+                prevSecond = currentSecond; // 이전 시간 업데이트
+
+                for (int i = 0; i < playerString.Length; i++)
+                {
+                    Console.SetCursorPosition(playerPos.X, playerPos.Y + i);
+                    Console.WriteLine(playerString[i]);
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.CursorVisible = false;
+
+            stopwatch.Start();
+            prevSecond = stopwatch.ElapsedMilliseconds;
+            bool isEscape = false;
+            while (!isEscape)
+            {
+                currentSecond = stopwatch.ElapsedMilliseconds;
+                ReadInput(out isEscape);
+                Render();
+            }
+        }
+    }
+}
